12691c75907c7d3f66d03c7a05c69e0e0ce1870e
WillsDesktop\Will <wsimkins2013@gmail.com>
2023-02-16 15:36:14 -0600

    icon

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -4,7 +4,7 @@ import ContactRight from "../components/Contact/ContactRight";
 
 const Contact = () => {
     return (
-        <main className="flex flex-col mx-4 gap-12 mt-12 xl:min-h-[27.5srem] lg:flex-row">
+        <main className="flex flex-col mx-4 gap-12 m2-6 xl:min-h-[27.5srem] lg:flex-row">
             <ContactCTA />
             <ContactRight />
         </main>
